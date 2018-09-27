08475def5c198c9844b9ad32e86b1852503d7f50
slavanza <zajch.slava@yandex.ru>
2018-09-27 19:34:59 +0300

    first commit

--- SetUp/main.cpp
+++ SetUp/main.cpp
@@ -1,12 +1,20 @@
 #include "stdafx.h"
 #include "cDictionary.h"
+#include "cNotFoundException.h"
+using namespace std;
 
 int main(int argc, char** argv)
 {
 	cDictionary<char, int> dictionary;
 	dictionary.Set('1', 1);
-	dictionary.IsSet('1');
-	dictionary.Get('1');
-	dictionary.Get('2');
+	cout << dictionary.IsSet('1') << endl;
+	cout << dictionary.Get('1') << endl;
+	try{
+		cout << dictionary.Get('2') << endl;
+	}
+	catch (cNotFoundException<char> e)
+	{
+		cout << e.GetKey() << endl;
+	}
 	return 0;
 }
